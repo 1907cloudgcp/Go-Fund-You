2f118a824be5db0b0959783efa939195affb676d
Abatson <abatson94@gmail.com>
2019-09-05 10:19:12 -0400

    working

---
+++ src/main/java/com/revature/controllers/ProjectController.java
@@ -0,0 +1,16 @@
+package com.revature.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+	
+	@GetMapping
+	public String findAll() {
+		return "Here's your projects";
+	}
+
+}
